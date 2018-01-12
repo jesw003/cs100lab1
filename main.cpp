a6c4a77c66456a49856ac2a1a6f05e2a7f3cbbec
Keyu Wang <kwang029@hammer.cs.ucr.edu>
2018-01-12 02:47:24 -0800

    added user input

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 
 #include <iostream>
-
+#include <string>
 int main ()
 {
-std::cout << "hello git" << std:: endl;
+std::string name;
+std::cout << "What is your name?" << std::endl;
+srd::cin>>name;
+std::cout<<"Hello " << name << "!" << std::endl;
 return 1;
 }
 
